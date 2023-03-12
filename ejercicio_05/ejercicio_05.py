d4c324b52c44536d05694ef7b3a4fb705da789df
luciacantos <luciacantos22@gmail.com>
2023-03-12 22:31:45 +0100

    def búsqueda dictómica

--- ejercicio_05/ejercicio_05.py
+++ ejercicio_05/ejercicio_05.py
@@ -1,6 +1,17 @@
 '''
 Ejercicio 5: Búsqueda por dicotomía en una tabla ordenada
 '''
+def búsqueda_dictómica(elemento, tabla, índice_min, índice_max):
+
+
+    if tabla[índice_min(tabla)] <= elemento <= tabla[índice_max(tabla)]:
+        if elemento < tabla[índice_min(tabla)] or elemento > tabla[índice_max(tabla)]:
+            return "Vacío"
+        else:
+            return búsqueda_dictómica_recursiva(elemento, tabla)
+    else:
+        return "Tabla no ordenada correctamente"
+
 
 def búsqueda_dictómica_recursiva(elemento, tabla_ordenada):
     len = len(tabla_ordenada)
